d488aa8f4a2d0a058e0150a9b18182b78d7fafa1
Sothiphak <sopheapsothiphak@gmail.com>
2025-10-18 10:06:20 +0700

    My first vue project

---
+++ e-commerce/src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div>
+    <header>
+      <h1>This is my first VueJs project</h1>
+
+      <nav>
+      </nav>
+    </header>
+
+    <RouterView />
+  </div>
+</template>
+
